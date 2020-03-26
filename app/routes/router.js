3e6d5078cb88d96fa6f038d7ffb533f6ba911ddf
qiubee <qiubees@hotmail.com>
2020-03-26 18:31:13 +0100

    add routes to create event & key pages

--- app/routes/router.js
+++ app/routes/router.js
@@ -3,7 +3,16 @@ const router = express.Router();
 
 // controllers
 const index = require("./controllers/index");
+const key = require("./controllers/key");
+const create = require("./controllers/create");
+const players = require("./controllers/players");
+const saveSport = require("./controllers/sport");
 
-router.get("/", index);
+router.get("/", index)
+    .get("/key", key.save)
+    .get("/enter-key", key.enter)
+    .get("/create", create)
+    .get("/players", players)
+    .post("/sport", saveSport);
 
 module.exports = router;
